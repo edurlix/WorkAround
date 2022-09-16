97ca8b5ff6636b6eff88801d4d20157150211c3f
edurlix <61432578+edurlix@users.noreply.github.com>
2022-09-16 14:30:35 -0400

    Update utilities.js

--- modules/utilities.js
+++ modules/utilities.js
@@ -2,7 +2,7 @@ export const formatNumber = (number) =>{
     let numStr = String(Math.floor(number));
     
     for(let i = numStr.length - 3; i > 0; i -= 3){
-        numStr = numStr.slice(0,3) + ',' + numStr.slice(i);
+        numStr = numStr.slice(0,i) + ',' + numStr.slice(i);
     }
 
     return numStr;
